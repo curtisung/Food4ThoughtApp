f2090e25dd01b3bd1167ca6ae7a50a9c0c095f2d
Curtis Ung <ungce@uci.edu>
2021-02-03 09:37:05 -0800

    Temp commit to temp repo

---
+++ app/src/main/java/com/example/foodapp/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.foodapp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Ingredient selection happens here.");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
