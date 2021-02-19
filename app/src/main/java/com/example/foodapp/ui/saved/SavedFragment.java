ce967ac32486022f1f10369899dc10d506c110c2
chchun2000 <christiansamuelchun@gmail.com>
2021-02-19 05:47:29 -0800

    added saved fragment pictures

--- app/src/main/java/com/example/foodapp/ui/saved/SavedFragment.java
+++ app/src/main/java/com/example/foodapp/ui/saved/SavedFragment.java
@@ -23,13 +23,13 @@ public class SavedFragment extends Fragment {
         savedViewModel =
                 new ViewModelProvider(this).get(SavedViewModel.class);
         View root = inflater.inflate(R.layout.fragment_saved, container, false);
-        final TextView textView = root.findViewById(R.id.text_saved);
-        savedViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_saved);
+//        savedViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
